25b48e15d4c5dcc909760d5295926480d3949e2f
Ahmad Nassri <ahmad@codeinchaos.com>
2016-03-02 02:36:12 -0500

    improved buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,5 +5,11 @@ particlesJS.load('particles-js', '/particles.json')
 $('.arrow-link').click(function (event) {
   event.preventDefault()
 
-  $('html, body').animate({scrollTop: $('#maincontent').offset().top - 50}, 1000)
+  $('html, body').animate({scrollTop: $($(event.target).attr('href')).offset().top - 50}, 1000)
+})
+
+$('#joinBtn').click(function (event) {
+  event.preventDefault()
+
+  $('html, body').animate({scrollTop: $($(event.target).attr('href')).offset().top - 50}, 1000)
 })
