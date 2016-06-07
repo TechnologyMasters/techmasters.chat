bf285b36b80f3c610f2920f8e3fd48ad5047182c
Ian Zamojc <izamojc@zamtools.com>
2016-06-07 01:08:54 -0400

    refactored to use IIFE

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,61 +1,72 @@
-// TODO: make this a config
-var apiKey     = 'AIzaSyD3Y_GF0P3MSCWZKhkMQ0FrUfKUf07m4U4';
-var calendarId = 'zamtools.com_sksehlk62uahqednpmvbp7isno@group.calendar.google.com';
+(function() {
+    // TODO: make this a config
+    var apiKey     = 'AIzaSyD3Y_GF0P3MSCWZKhkMQ0FrUfKUf07m4U4';
+    var calendarId = 'zamtools.com_sksehlk62uahqednpmvbp7isno@group.calendar.google.com';
 
-// change delimiters to not conflict with Jekyll templates
-Vue.config.delimiters = ['[[', ']]'];
+    // change delimiters to not conflict with Jekyll templates
+    Vue.config.delimiters = ['[[', ']]'];
 
-Vue.filter('prettyDate', function(value) {
-    var weekdays = ['Monday', 'Tuesday', 'Wedday', 'Thursday', 'Friday', 'Satday', 'Sunday'];
-    var months   = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    Vue.filter('prettyDate', function(value) {
+        var weekdays = ['Monday', 'Tuesday', 'Wedday', 'Thursday', 'Friday', 'Satday', 'Sunday'];
+        var months   = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    var d = new Date(value);
-    var weekday     = weekdays[d.getDay()];
-    var month       = months[d.getMonth()];
-    var twelveHours = ((d.getHours() + 11) % 12 + 1);
-    var minutes     = ('0' + d.getMinutes()).slice(-2);
-    var period      = (d.getHours() >= 12 ? 'PM' : 'AM');
-    return '' + weekday + ', ' + month + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + twelveHours + ':' + minutes + ' ' + period;
-});
+        var d = new Date(value);
+        var weekday     = weekdays[d.getDay()];
+        var month       = months[d.getMonth()];
+        var twelveHours = ((d.getHours() + 11) % 12 + 1);
+        var minutes     = ('0' + d.getMinutes()).slice(-2);
+        var period      = (d.getHours() >= 12 ? 'PM' : 'AM');
+        return '' + weekday + ', ' + month + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + twelveHours + ':' + minutes + ' ' + period;
+    });
 
-Vue.filter('googleMapUrl', function(value) {
-    return 'http://maps.google.com/?q=' + encodeURIComponent(value);
-});
+    Vue.filter('googleMapUrl', function(value) {
+        return 'http://maps.google.com/?q=' + encodeURIComponent(value);
+    });
 
-var vue = new Vue({
-    el: '#app',
-    data: {
-        events: []
-    }
-});
-
-var getJson = function(url, success, failure) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.onload = function() {
-        if (this.status >= 200 && this.status < 400) {
-            success(JSON.parse(this.response));
+    var vue = new Vue({
+        el: '#app',
+        data: {
+            events: []
         }
+    });
+
+    var getJson = function(url, params, success, failure) {
+        url += '?';
+        for (var attr in params) {
+            url += attr + '=' + params[attr];
+        }
+
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                success(JSON.parse(this.response));
+            }
+        };
+
+        request.onerror = function(err) {
+            failure(err);
+        };
+
+        request.send();
     };
 
-    request.onerror = function(err) {
-        failure(err);
-    };
+    document.addEventListener('DOMContentLoaded', function() {
+        var url = 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events';
+        var params = {
+            key: apiKey
+        };
 
-    request.send();
-};
+        getJson(url, params,
+            function(data) {
+                // TODO: reverse order for descending dates
+                vue.$data.events = data.items;
+            },
+            function(err) {
+                console.log('error', err);
+            }
+        );
+    });
+})();
 
-document.addEventListener('DOMContentLoaded', function() {
-    var url = 'https://www.googleapis.com/calendar/v3/calendars/' + calendarId + '/events?key=' + apiKey;
-
-    getJson(url,
-        function(data) {
-            // TODO: reverse order for descending dates
-            vue.$data.events = data.items;
-        },
-        function(err) {
-            console.log('error', err);
-        }
-    );
-});
