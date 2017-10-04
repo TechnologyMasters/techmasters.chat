2113b169aadfb697a03d4ba44ac48444997dfbe5
Enrico Sacchetti <enrico@theetrain.ca>
2017-10-03 21:44:14 -0400

    feat(jobs): add showdown
    - for markdown formatting in expandable job details

---
+++ assets/js/jobs.js
@@ -0,0 +1,111 @@
+/* globals showdown */
+
+var sdConverter = new showdown.Converter()
+
+var JobBoard = (function () {
+  // Data
+  var api = {
+    endpoint: 'https://api.github.com/repos/TechnologyMasters/jobs/issues',
+    headers: {
+      Accept: 'application/vnd.github.v3+json'
+    }
+  }
+  var jobs = null
+
+  // Components
+  var jobTable = (function (el) {
+    var element = el
+
+    return {
+      close: function () {
+        element.style.display = 'none'
+      },
+      open: function () {
+        element.style.display = 'table'
+      }
+    }
+  })(document.getElementById('job-table'))
+
+  var jobBody = document.getElementById('job-body')
+  var jobItem = function (element, index) {
+    var date = new Date(element.created_at)
+    var dateOptions = {
+      year: "numeric", month: "short", day: "numeric"
+    }
+    var postedDate = date.toLocaleDateString("en-US", dateOptions)
+
+    var labels = ''
+    element.labels.forEach(function(label) {
+      labels += '\
+        <span class="mdl-chip">\
+          <span class="mdl-chip__text">' + label.name + '</span>\
+        </span>'
+    })
+
+    return '\
+      <tr role="button" tabindex="0" data-index="' + index + '" onClick="JobBoard.expand(this)">\
+        <td class="mdl-data-table__cell--non-numeric">\
+          ' + element.title + '\
+        </td>\
+        <td class="mdl-data-table__cell--non-numeric show-tablet-up">\
+          ' + postedDate + '\
+        </td>\
+        <td class="mdl-data-table__cell--non-numeric show-desktop-up">\
+          ' + labels + '\
+        </td>\
+      </tr>'
+  }
+
+  var jobDescription = (function (el, body) {
+    var element = el
+    var elBody = body
+
+    return {
+      set: function (html) {
+        elBody.innerHTML = html
+        return this
+      },
+      close: function () {
+        element.style.display = 'none'
+      },
+      open: function () {
+        element.style.display = 'block'
+      }
+    }
+  })(document.getElementById('tm-jobs-description'), document.getElementById('tm-jobs-description_body'))
+
+  // Get jobs json
+  var request = new XMLHttpRequest()
+  request.open('GET', api.endpoint)
+  request.setRequestHeader('Accept', api.headers.Accept)
+  request.onload = function () {
+    if (request.status >= 200) {
+      jobs = JSON.parse(request.responseText)
+      setupJobs()
+    }
+  }
+
+  request.send()
+
+  // Create paginated list
+  var setupJobs = function () {
+    var jobCollection = ''
+
+    jobs.forEach(function (element, index) {
+      jobCollection += jobItem(element, index)
+    });
+
+    jobBody.innerHTML = jobCollection
+  }
+
+  return {
+    expand: function (el) {
+      jobDescription.set(sdConverter.makeHtml(jobs[el.dataset.index].body)).open()
+      jobTable.close()
+    },
+    collapse: function () {
+      jobDescription.close()
+      jobTable.open()
+    }
+  }
+})()
